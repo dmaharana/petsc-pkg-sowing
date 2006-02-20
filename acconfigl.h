483e6660581131ca034fda4945fe7173c61e125c
gropp <gropp@43f3db66-2a03-4d6b-8051-32b12e86fc3a>
2006-02-20 15:01:26 +0000

    Removed derived files and switch to new AC_INIT form
    git-svn-id: https://svn.mcs.anl.gov/repos/sowing/trunk@121 43f3db66-2a03-4d6b-8051-32b12e86fc3a

--- acconfigl.h
+++
@@ -1,5 +0,0 @@
-/* Define this to enable the memory debugging code in tr.c */
-#undef MEMORY_TRACING
-
-/* Define this to require use of the memory tracing routines */
-#undef MEMORY_TRACING_REQUIRED
