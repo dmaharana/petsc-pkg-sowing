5d0adffd965a3b2b22f67ebbc59552c2c07b9e1e
Barry Smith <bsmith@mcs.anl.gov>
2015-01-16 21:08:31 -0600

    Another missing file from Bill's update

---
+++ include/rdconfig.h
@@ -0,0 +1,22 @@
+#ifndef RDCONFIG_H_INCLUDED
+#define RDCONFIG_H_INCLUDED
+
+typedef struct _SYConfigCmds {
+    const char *name;
+    int (*docmd)(const char *name, const char *key, const char *value,
+		 void *extra);
+    void *cmdextra;
+} SYConfigCmds;
+
+int SYReadConfigFile(const char filename[], const char sepChar,
+		     const char commentChar,
+		     SYConfigCmds *cmds, int ncmds);
+int SYConfigDBInit(const char *cmd, void *extra);
+int SYConfigDBInsert(const char *cmd, const char *key, const char *value,
+		     void *extracmd);
+int SYConfigDBLookup(const char *cmd, const char *key, const char **value,
+		     void *extracmd);
+int SYConfigDBFree(void *extracmd);
+int SYConfigDBPrint(void *extracmd, FILE *fp);
+
+#endif /* RDCONFIG_H_INCLUDED */
