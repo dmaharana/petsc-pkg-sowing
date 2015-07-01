ae2d6c42c2599699d6d146f9769e9fee77519ea4
gropp <gropp@43f3db66-2a03-4d6b-8051-32b12e86fc3a>
2015-07-01 08:56:11 +0000

    Improve tests for doctext
    git-svn-id: https://svn.mcs.anl.gov/repos/sowing/trunk@214 43f3db66-2a03-4d6b-8051-32b12e86fc3a

---
+++ src/doctext/test/tststra.h
@@ -0,0 +1,10 @@
+/*M mystruct - structure of pointers
+
+Synopsis:
+   int *pointer;
+   int n;
+   for (i=0; i<n; i++) { pointer_array[i] = area to fill; }
+   This line tests special characters: $, |, \, &, <>, 't', "text"
+
+This is the end, and is `not` part of the synopsis.
+M*/
