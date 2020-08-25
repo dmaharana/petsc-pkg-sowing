674d5f3c1bf3ce6e1d744818915a0d723ce4dbe0
Satish Balay <balay@mcs.anl.gov>
2020-08-24 22:44:41 -0500

    fix build error on freebsd

--- include/daytime.h
+++ include/daytime.h
@@ -2,6 +2,6 @@
 #define _DAYTIME
 
 extern void SYGetDate(char *);
-extern void SYGetDayTime(struct timeval *);
+/* extern void SYGetDayTime(struct timeval *); Unused and breaks bsd build */
 #endif
 
